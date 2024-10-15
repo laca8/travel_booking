e2d2c801751fbd2fa1d92f1ccb1ed758b48508be
Ragab <89777423+laca8@users.noreply.github.com>
2024-10-15 21:33:07 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,37 +5,31 @@ const cors = require("cors");
 const globalError = require("./middlewares/errorhandler");
 dotenv.config({ path: "./config/config.env" });
 const connDb = require("./config/db");
-const reportRoute = require("../routes/reportRoute");
-const suggRoute = require("../routes/suggestionRoute");
-const noteRoute = require("../routes/noteRoute");
-const userRoute = require("../routes/userRoute");
-const noteSuggRoute = require("../routes/noteSuggRoute");
+const reportRoute = require("./routes/reportRoute");
+const suggRoute = require("./routes/suggestionRoute");
+const noteRoute = require("./routes/noteRoute");
+const userRoute = require("./routes/userRoute");
+const noteSuggRoute = require("./routes/noteSuggRoute");
 const ApiError = require("./utils/apiError");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-app.use(express.static("clients/dist"));
+app.use(cors({ origin: "*", credentials: true }));
 connDb();
 app.use("/api/report", reportRoute);
 app.use("/api/sugg", suggRoute);
 app.use("/api/note", noteRoute);
 app.use("/api/user", userRoute);
 app.use("/api/note/sugg", noteSuggRoute);
-app.use("/api/eduLevel", require("../routes/admin/eduLevel"));
-app.use("/api/eduStatus", require("../routes/admin/eduStatus"));
-app.use("/api/marrideStatus", require("../routes/admin/marrideStatus"));
-app.use("/api/outWork", require("../routes/admin/outWork"));
-app.use("/api/workType", require("../routes/admin/workType"));
-app.use("/api/disable", require("../routes/admin/disable"));
-app.use("/api/city", require("../routes/admin/city"));
-app.use("/api/place", require("../routes/admin/place"));
-app.use("/api/shiek", require("../routes/admin/shiek"));
+app.use("/api/eduLevel", require("./routes/admin/eduLevel"));
+app.use("/api/eduStatus", require("./routes/admin/eduStatus"));
+app.use("/api/marrideStatus", require("./routes/admin/marrideStatus"));
+app.use("/api/outWork", require("./routes/admin/outWork"));
+app.use("/api/workType", require("./routes/admin/workType"));
+app.use("/api/disable", require("./routes/admin/disable"));
+app.use("/api/city", require("./routes/admin/city"));
+app.use("/api/place", require("./routes/admin/place"));
+app.use("/api/shiek", require("./routes/admin/shiek"));
 app.use("*", (req, res, next) => {
   next(new ApiError("this route not found", 404));
 });
